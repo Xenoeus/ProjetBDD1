5ea4a545251873111f87d319d31ac790fb62d49c
pierre503 <pierre503@live.be>
2016-12-14 22:52:48 +0100

    erreur s'il rentre une mauvaise base de donnée
    donc s'il rentre une mauvaise base de donnée ou s'il se trompe ça ralera
    comme quoi ce n'est pas une base de donnée et donc il saura qu'il a mal
    rentré les informations.

--- Relation.py
+++ Relation.py
@@ -1,10 +1,28 @@
+import sqlite3
 class Relation:
    """
    Cette classe represente une relation en SPJRUD ou une table en SQL.
    """
+
+   def verification(self,nom):
+      connection = sqlite3.connect('test.db')
+      c = connection.cursor()
+      c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+      for i in c:
+         if(i[0]== nom):
+            connection.close()
+            return False
+
+      connection.close()
+      return True
+   
    def __init__(self, nom, attributs):
       self.nom = nom
       self.attributs = attributs
-
+      if (self.verification(nom)):
+         print(nom)
+         print("Ce nom de relation n'est pas correct ou cette relation n'existe pas")
    def __str__(self):
       return self.nom
+
+
